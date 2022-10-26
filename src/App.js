5a2420dbfc215a37949bd9c3db274caa50cc1619
Satyam <satyam19arya@gmail.com>
2022-10-26 23:00:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Expenses from './components/Expenses';
 
-function App() {
+function App(){
+    let expenses = [   // we have made an array of objects
+    {
+      id: 'e1',
+      title: 'school fee',
+      amount: 900,
+      date: new Date(2021,5 ,7)
+    },
+    {
+      id: 'e2',
+      title: 'car fee',
+      amount: 500,
+      date: new Date(2022,2 ,7)
+    },
+    {
+      id: 'e3',
+      title: 'bus fee',
+      amount: 800,
+      date: new Date(2021,3 ,7)
+    },
+    {
+      id: 'e4',
+      title: 'food fee',
+      amount: 100,
+      date: new Date(2022,4 ,7)
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h3>Expense notepad</h3>
+      <Expenses item={expenses}/>
     </div>
   );
 }
